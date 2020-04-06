9536f13170caeeacc4c59d645baa59b7ab424564
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-06 17:39:32 +0300

    работа с state. добавил кнопку изменения логотипа

--- app_1/src/nav/Nav.js
+++ app_1/src/nav/Nav.js
@@ -3,10 +3,30 @@ import './Nav.css';
 
 
 class Nav extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			logo: true
+		}
+	}
+
+	changeLogo = () => {
+		this.setState(state => ({
+			logo: !state.logo
+
+		}));
+
+
+	};
+
 	render() {
 		const nav = this.props.nav;
 		return (
-			<nav>
+			<nav className={'nav nav-block'}>
+				<div className="logo-block">
+					<a className={'logo'} href={''}>{this.state.logo ? 'Learn React' : 'React learned!)'}</a>
+					<button className={"btn btn-secondary  ml-3"} onClick={this.changeLogo}>Сhange logo</button>
+				</div>
 				<ul className={'nav'}>
 					{Object.keys(nav).map((elem, i) => {
 						return <li key={i} className={'nav-item'}><a href={nav[elem]}>{elem}</a></li>
