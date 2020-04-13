80f8f985bd08b8ae67bdc31bcf6d6e7f24d8fdb3
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-13 22:06:09 +0300

    счетчик с помощю useState

--- app_2/src/App.js
+++ app_2/src/App.js
@@ -1,19 +1,33 @@
-import React, {Component} from 'react';
+//
+import React, {useState} from 'react';
 import './App.sass';
 
-class App extends Component {
-	constructor() {
-		super();
-
-	}
-
-	render() {
-		return (
-			<div className="App">
-
+function App() {
+	const values = [5, -5];
+	const [count, changeCount] = useState(0);
+	return (
+		<div className="App">
+			<div className="uk-card uk-card-default uk-card-body">
+				<h3 className="uk-card-title">Counter</h3>
+				<div>{count}</div>
+				<button className="uk-button uk-button-secondary"
+						  onClick={() => changeCount(
+							  count + +values.filter(item => item > 0)
+						  )}>+
+				</button>
+				<button className="uk-button uk-button-primary"
+						  onClick={() => changeCount(
+							  count + +values.filter(item => item < 0)
+						  )}>-
+				</button>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
+
 export default App;
+
+
+
+
