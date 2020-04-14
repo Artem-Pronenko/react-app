7d4f4fd674a5918094e164632e0aa0b758659248
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-14 15:54:10 +0300

    добавил папку build

---
+++ app_1/build/precache-manifest.19defd89b8a4ad3e7aff490a94f2c004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5500e3cfeafb05a4d0646e1450e084b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059626e766ca66269caf",
+    "url": "/static/css/main.6acacab4.chunk.css"
+  },
+  {
+    "revision": "11f32518e73d5ce68387",
+    "url": "/static/js/2.eb00508f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb00508f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059626e766ca66269caf",
+    "url": "/static/js/main.d3342c53.chunk.js"
+  },
+  {
+    "revision": "db9323a42088b5e07cf9",
+    "url": "/static/js/runtime-main.dbed06e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
