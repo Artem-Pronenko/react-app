8138581cfffce3542527d3dd7072c0f6fcf8b79f
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-14 15:00:05 +0300

    Накидал сетку на grid + базовая разметка + разбил на компоненты

---
+++ app_2/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.sass'
+
+const Header = () => {
+	return (
+		<header className={'header'}>
+			<div className="container">
+				<a href="" className="logo-go-hom">
+					<div className="logo">НЕ Вконтакте</div>
+				</a>
+			</div>
+		</header>
+	)
+};
+
+
+export default Header;
+
+
+
+
---
+++ app_2/src/components/nav/Nav.jsx
@@ -0,0 +1,24 @@
+//
+import React from 'react';
+import './Nav.sass'
+
+const Nav = () => {
+	return (
+		<nav className={'nav'}>
+			<ul className="nav-list">
+				<li className="nav-item"><a href="">Profile</a></li>
+				<li className="nav-item"><a href="">Messages</a></li>
+				<li className="nav-item"><a href="">News</a></li>
+				<li className="nav-item"><a href="">Music</a></li>
+				<li className="nav-item"><a href="">Settings</a></li>
+			</ul>
+		</nav>
+	)
+};
+
+
+export default Nav;
+
+
+
+
