f1c5d29351a039458813f99b663eeb5d7eb5b27a
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-14 15:41:52 +0300

    Накидал сетку на grid + базовая разметка + разбил на компоненты

--- app_2/src/App.js
+++ app_2/src/App.js
@@ -1,26 +1,19 @@
 //
-import React, {useState} from 'react';
+import React from 'react';
 import './App.sass';
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Profile from "./components/Profile";
 
 function App() {
-	const values = [5, -5];
-	const [count, changeCount] = useState(0);
 	return (
-		<div className="App">
-			<div className="uk-card uk-card-default uk-card-body">
-				<h3 className="uk-card-title">Counter</h3>
-				<div>{count}</div>
-				<button className="uk-button uk-button-secondary"
-						  onClick={() => changeCount(
-							  count + +values.filter(item => item > 0)
-						  )}>+
-				</button>
-				<button className="uk-button uk-button-primary"
-						  onClick={() => changeCount(
-							  count + +values.filter(item => item < 0)
-						  )}>-
-				</button>
+		<div className="app-wrapper">
+			<Header/>
+			<div className="container">
+				<Nav/>
+				<Profile/>
 			</div>
+
 		</div>
 	)
 }
