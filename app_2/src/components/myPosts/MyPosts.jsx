58c5f80354500a8de8282df7f40676b33689f2cf
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-16 22:02:56 +0300

    вывел посты через map

--- app_2/src/components/myPosts/MyPosts.jsx
+++ app_2/src/components/myPosts/MyPosts.jsx
@@ -5,7 +5,53 @@ import postImg from './post/postImg.jpg';
 import postImg2 from './post/postImg2.jpg';
 import postImg3 from './post/postImg3.jpg';
 
+const dataPost = [
+	{
+		id: 0,
+		date: '6 iюл. 2019',
+		imgUrl: postImg3,
+		like: 4,
+		comments: 5,
+		views: 99,
+		explain: 'обновилъ фотокарточку въ паспортѣ:', text: ''
+	},
+	{
+		id: 1,
+		date: '24 iюн. 2019',
+		imgUrl: postImg2,
+		like: 7,
+		comments: 2,
+		views: 104,
+		explain: 'обновилъ фотокарточку въ паспортѣ:', text: ''
+	},
+	{
+		id: 2,
+		date: '17 iюн. 2019',
+		imgUrl: postImg,
+		like: 1,
+		comments: '',
+		views: 68,
+		explain: '',
+		text: 'Действительно годный эвент. трёхдневная конференция от WebDev Summit окончена, было действительно круто. очень много полезной информации, от профессионалов, очень плотная подача. было очень интересно и круто. Попытаюсь попасть к вам на курсы. Жду следующего ивента. советую всем.#webdevsummit #webdevsummit2019'
+	},
+];
+
 const MyPosts = () => {
+
+	const posts = dataPost.map((
+		{date, text, imgUrl, like, comments, views, explain}
+	) => {
+		return <Post
+			date={date}
+			text={text}
+			imgUrl={imgUrl}
+			like={like}
+			comments={comments}
+			views={views}
+			explain={explain}
+		/>
+	});
+
 	return (
 		<div className={c.profile_post}>
 			<div className={c.create_post}>
@@ -14,35 +60,7 @@ const MyPosts = () => {
 				</form>
 			</div>
 
-			<Post
-				date={'6 iюл. 2019'}
-				text={``}
-				imgUrl={postImg3}
-				like={4}
-				comments={5}
-				views={99}
-				explain={'обновилъ фотокарточку въ паспортѣ:'}
-			/>
-			<Post
-				date={'24 iюн. 2019'}
-				text={``}
-				imgUrl={postImg2}
-				like={7}
-				comments={2}
-				views={104}
-				explain={'обновилъ фотокарточку въ паспортѣ:'}
-			/>
-
-			<Post
-				date={'17 iюн. 2019'}
-				text={`Действительно годный эвент. трёхдневная конференция от WebDev Summit окончена, было действительно круто. очень много полезной информации, от профессионалов, очень плотная подача. было очень интересно и круто. Попытаюсь попасть к вам на курсы. Жду следующего ивента. советую всем.#webdevsummit #webdevsummit2019`}
-				imgUrl={postImg}
-				like={1}
-				comments={''}
-				views={68}
-				explain={''}
-			/>
-
+			{posts}
 
 		</div>
 	)
