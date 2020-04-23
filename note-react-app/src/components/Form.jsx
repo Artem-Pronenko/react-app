632bfbaf9130519fd145c49b701e19aac420e143
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-23 13:52:19 +0300

    show/hide alert

--- note-react-app/src/components/Form.jsx
+++ note-react-app/src/components/Form.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import {AlertContext} from "../context/alert/alertContext";
 
 export const Form = () => {
+
+	const [value, setValue] = useState('');
+	const alert = useContext(AlertContext);
+
+	const submitHandler = event => {
+		event.preventDefault();
+
+
+		if (value.trim()) {
+			alert.show('Заметка создана!', 'success');
+			setValue('');
+		} else {
+			alert.show('Название заметки не может быть пустым!', 'warning');
+		}
+
+	};
+
 	return (
-		<form action="">
+		<form action="" onSubmit={submitHandler}>
 			<div className="form-group">
 				<input
 					type="text"
 					className="form-control"
 					placeholder={'Название заметки'}
+					value={value}
+					onChange={e => setValue(e.target.value)}
 				/>
 			</div>
 		</form>
